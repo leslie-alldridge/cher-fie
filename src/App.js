d2970f22ce1fabf68bb6d7242ff291602226425a
chloe <chloeseunpark@gmail.com>
2019-04-10 17:32:25 +1200

    add image input form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import ImageInputForm from './ImageInputForm'
+import './App.css'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className='App'>
+        <ImageInputForm />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
---
+++ src/ImageInputForm.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ImageInputForm = () => {
+  return (
+    <div>
+      <input type='text' />
+      <button>Cher-ify</button>
+    </div>
+  )
+}
+
+export default ImageInputForm
