690f2a51d813550b773827ff291c47d5ec64e504
chloe <chloeseunpark@gmail.com>
2019-04-24 23:23:47 +1200

    add more cher

--- src/Components/cher.js
+++ src/Components/cher.js
@@ -3,5 +3,8 @@ import cher2 from '../media/cher/2.png'
 import cher3 from '../media/cher/3.png'
 import cher4 from '../media/cher/4.png'
 import cher5 from '../media/cher/5.png'
+import cher6 from '../media/cher/6.png'
+import cher7 from '../media/cher/7.png'
+import cher8 from '../media/cher/8.png'
 
-export const cher = [cher1, cher2, cher3, cher4, cher5]
+export const cher = [cher1, cher2, cher3, cher4, cher5, cher6, cher7, cher8]
