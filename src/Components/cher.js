526368d30ba21a23ae81e9d288c818974a863ca2
chloe <chloeseunpark@gmail.com>
2019-04-24 23:06:12 +1200

    add more cher images

--- src/Components/cher.js
+++ src/Components/cher.js
@@ -1,5 +1,7 @@
 import cher1 from '../media/cher/1.png'
 import cher2 from '../media/cher/2.png'
 import cher3 from '../media/cher/3.png'
+import cher4 from '../media/cher/4.png'
+import cher5 from '../media/cher/5.png'
 
-export const cher = [cher1, cher2, cher3]
+export const cher = [cher1, cher2, cher3, cher4, cher5]
